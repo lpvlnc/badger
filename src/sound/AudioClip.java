8e1eda38ef95ef049885e431fa13dac22255ffad
Leonardo Pinheiro Valença <lpvalenca@hotmail.com>
2021-11-21 22:41:08 -0300

    sons

--- src/sound/AudioClip.java
+++ src/sound/AudioClip.java
@@ -5,10 +5,27 @@
  */
 package sound;
 
+import java.io.File;
+import java.io.IOException;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 /**
  *
  * @author Leonardo
  */
 public class AudioClip {
+    private File file;
     
+    public AudioClip(String path){
+        file = new File(path);
+        if(!file.exists()){
+            System.out.println("ERROR >> AudioClip Not Found!");
+        }
+    }
+    
+    public AudioInputStream getAudioStream() throws UnsupportedAudioFileException, IOException{
+        return AudioSystem.getAudioInputStream(file);
+    }
 }
--- src/sound/AudioPlayer.java
+++ src/sound/AudioPlayer.java
@@ -5,10 +5,79 @@
  */
 package sound;
 
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
+
 /**
  *
  * @author Leonardo
  */
 public class AudioPlayer {
+    public static synchronized void play(AudioClip sfx, double volume){
+ 
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                AudioInputStream stream;
+                try {
+                    
+                    stream = sfx.getAudioStream();
+                    Clip clip;
+                    
+                    try {
+                        
+                        clip = AudioSystem.getClip();
+                        clip.open(stream);
+                        setVolume(clip, volume);
+                        clip.start();
+                        
+                    } catch (LineUnavailableException ex) {
+                        Logger.getLogger(AudioPlayer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } catch (UnsupportedAudioFileException | IOException ex) {
+                    Logger.getLogger(AudioPlayer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }; thread.start();
+    }
     
+    public static synchronized void loop(AudioClip sfx, double volume){
+        Thread thread = new Thread(){
+            @Override
+            public void run(){
+                AudioInputStream stream;
+                try {
+                    
+                    stream = sfx.getAudioStream();
+                    Clip clip;
+                    
+                    try {
+                        
+                        clip = AudioSystem.getClip();
+                        clip.open(stream);
+                        setVolume(clip, volume);
+                        clip.loop(Clip.LOOP_CONTINUOUSLY);
+                        
+                    } catch (LineUnavailableException ex) {
+                        Logger.getLogger(AudioPlayer.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+                } catch (UnsupportedAudioFileException | IOException ex) {
+                    Logger.getLogger(AudioPlayer.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+        }; thread.start();
+    }
+    
+    private static void setVolume(Clip clip, double volume){
+        FloatControl gain = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
+        float dB = (float)(Math.log(volume) / Math.log(10) * 20);
+        gain.setValue(dB);
+    }
 }
